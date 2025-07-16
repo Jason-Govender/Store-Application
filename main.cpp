235e40909fa9151763d4e44bfa193b4b56809181
Jason Govender <jasongovend@gmail.com>
2025-07-16 21:15:33 +0200

    Implemented a splash screen and application icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QPixmap>
+#include <QTimer>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icon.png"));
+    QPixmap pixmap(":/splash.png");
+    qDebug() << "Is splash image valid? " << !pixmap.isNull();
+    QSplashScreen splash(pixmap);
+    splash.show();
+    app.processEvents();
+    QTimer::singleShot(2000, &splash, &QSplashScreen::close);
     MainWindow w;
-    //Hello World!
-    w.show();
-    return a.exec();
+    QTimer::singleShot(2000, [&]() {
+        splash.finish(&w);
+        w.show();
+    });
+    return app.exec();
 }
