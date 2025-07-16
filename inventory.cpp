559850a3d5e818a62638f942ae108173ce5066bb
Jason Govender <jasongovend@gmail.com>
2025-07-16 16:34:14 +0200

    Ensured memory deallocation.

--- inventory.cpp
+++ inventory.cpp
@@ -4,10 +4,32 @@ Inventory::Inventory(QList <Item*> items)
     :m_items(items)
 {}
 
+Inventory::~Inventory(){
+    for(Item* item : m_items){
+        delete item;
+    }
+}
+
 void Inventory::addItem(Item* item){
     m_items.append(item);
 }
 
 void Inventory::processTransaction(const Transaction& transaction){
+    int inStock = 0;
+    for (Item* item : m_items){
+        if(item == transaction.getItem())
+        ++inStock;
+    }
 
+    if(inStock>=transaction.getQuantity()){
+        for (int i=m_items.size(); i>=0 && inStock>0; --i){
+            if (m_items[i] == transaction.getItem()){
+                delete m_items[i];
+                m_items.removeAt(i);
+                --inStock;
+            }
+        }
+    }
+    else
+        qDebug() << "The item and/or quantity is not in stock.";
 }
--- inventory.h
+++ inventory.h
@@ -6,11 +6,14 @@ class Inventory
 {
 public:
     Inventory(QList<Item*> items);
+    ~Inventory();
+    QList<Customer*> getCustomers();
     void addItem(Item* item);
     void processTransaction(const Transaction& transaction);
 
 private:
     QList<Item*> m_items;
+    QList<Customer*> m_customers;
 };
 
 #endif // INVENTORY_H
